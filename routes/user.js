1ac05abafb21210f6deb4d398cc41ea998eee4a9
apple <apple@PWM-CO-18777.local>
2025-04-02 07:54:17 +0530

    removed auth middleware for users module

--- routes/user.js
+++ routes/user.js
@@ -4,11 +4,11 @@ const auth = require('../middlewares/auth');
  
 const router = express.Router()
 
-router.post('/create', auth, userController.create)
-router.get('/list', auth, userController.list)
-router.delete('/:id', auth, userController.deleteUser)
-router.get('/:id', auth, userController.getUserDetails)
-router.put('/:id', auth, userController.updateUserDetails)
+router.post('/create', userController.create)
+router.get('/list', userController.list)
+router.delete('/:id', userController.deleteUser)
+router.get('/:id', userController.getUserDetails)
+router.put('/:id', userController.updateUserDetails)
 router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.post('/admin/login', userController.adminLogin)
